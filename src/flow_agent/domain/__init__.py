133702f5e691e60cf69dff337159066dc4101a87
var-666 <2483727663@qq.com>
2026-09-23 11:29:19 +0800

    feat: 导出领域模型公共 API

---
+++ src/flow_agent/domain/__init__.py
@@ -0,0 +1,14 @@
+from flow_agent.domain.artifact import Artifact, ArtifactKind
+from flow_agent.domain.goal import Goal
+from flow_agent.domain.run import Run, RunStatus
+from flow_agent.domain.task import Task, TaskStatus
+
+__all__ = [
+    "Artifact",
+    "ArtifactKind",
+    "Goal",
+    "Run",
+    "RunStatus",
+    "Task",
+    "TaskStatus",
+]
